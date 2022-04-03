8b4a5d54ab7fc144f3bb13954ea40b80df32fa55
visa423 <53490935+visa423@users.noreply.github.com>
2022-04-02 20:10:46 -0400

    Euler 1 Upload

---
+++ euler_1.py
@@ -0,0 +1,19 @@
+"""
+This algorithm first adds all the multiples of three
+to the running total, and then adds all the multiples
+of five PROVIDED they are not also a multiple of three.
+"""
+
+def algorithm():
+    runningTotal = 0
+    # Add all multiples of three
+    for i in range (1, 334): # range is [a,b)
+        runningTotal += i * 3
+    # Add all multiples of five, PROVIDED they are not a multiple of three
+    for i in range (1, 200):
+        if i % 3 != 0:
+            runningTotal += i * 5
+    
+    print(runningTotal)
+
+algorithm()
